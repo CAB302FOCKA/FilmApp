12794c58b36cc70c82148dfe5284fd79c6fece20
Westbrooke117 <kyhaanderson@gmail.com>
2025-04-11 13:42:18 +1000

    Populated HttpRequest class

--- src/main/java/com/example/filmapp/HttpRequest.java
+++ src/main/java/com/example/filmapp/HttpRequest.java
@@ -1,4 +1,34 @@
 package com.example.filmapp;
 
-public class HttpRequest {
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+
+public class HttpRequest
+{
+    String url;
+
+    public HttpRequest(String url) {
+        this.url = url;
+    }
+
+    public JSONObject Fetch() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjFhNDZmZTAzMDYzMTkxYzIxZmRlNDFjZDNjNGY3YSIsIm5iZiI6MTc0MjkwNDcyNS44MjMsInN1YiI6IjY3ZTI5ZDk1MTZhM2M1YzIyNGYwNjUzMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XkcGqKO9RPDcTHisYqAWnXG__T3ZQuggbDABN8xYv5Q")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        return (JSONObject) JSONValue.parse(response.body().string());
+    }
 }
