2b470021205e7aa2f31407482ae89285dc1b1a5a
kyhaa <kyhaanderson@gmail.com>
2025-04-11 22:29:01 +1000

    Finished first pass of API class with methods for each required API endpoint

--- src/main/java/com/example/filmapp/API.java
+++ src/main/java/com/example/filmapp/API.java
@@ -6,25 +6,26 @@ import java.io.IOException;
 import java.text.MessageFormat;
 
 public class API {
-    public JSONObject getSeriesOverview(String titleQuery) throws IOException {
-        String url = MessageFormat.format("https://api.themoviedb.org/3/search/tv?query={0}&include_adult=false&language=en-US&page=1", titleQuery);
+    public JSONArray searchMediaByTitle(String titleQuery, String mediaType) throws IOException {
+        String url = MessageFormat.format("https://api.themoviedb.org/3/search/{0}?query={1}&include_adult=false&language=en-US&page=1", mediaType, titleQuery);
         HttpRequest httpRequest = new HttpRequest(url);
         JSONObject jsonResponse = httpRequest.Fetch();
 
-        JSONArray results  = (JSONArray) jsonResponse.get("results");
-        return (JSONObject) results.getFirst();
+        return (JSONArray) jsonResponse.get("results");
     }
 
-    public String getSeriesImage(Long id) throws IOException {
-        String url = MessageFormat.format("https://api.themoviedb.org/3/tv/{0,number,#}/images?language=en", id);
+    public JSONObject getMediaDetails(String mediaType, String id) throws IOException {
+        String url = MessageFormat.format("https://api.themoviedb.org/3/{0}/{1}", mediaType, id);
+        HttpRequest httpRequest = new HttpRequest(url);
+
+        return httpRequest.Fetch();
+    }
+
+    public JSONArray getTrendingMediaList(String mediaType) throws IOException {
+        String url = MessageFormat.format("https://api.themoviedb.org/3/trending/{0}/day", mediaType);
         HttpRequest httpRequest = new HttpRequest(url);
         JSONObject jsonResponse = httpRequest.Fetch();
 
-        JSONArray results  = (JSONArray) jsonResponse.get("posters");
-        JSONObject firstResult = (JSONObject) results.getFirst();
-
-        String posterImageURL = (String) firstResult.get("file_path");
-
-        return (MessageFormat.format("https://image.tmdb.org/t/p/w500{0}", posterImageURL));
+        return (JSONArray) jsonResponse.get("results");
     }
 }
