ecc3b2cc5516b7d22ec0112af23a1294afc84db2
Charlie Nielsen <charlielnielsen@hotmail.com>
2025-04-22 13:15:11 +1000

    attempt to merge

--- src/main/java/com/example/filmapp/HelloApplication.java
+++ src/main/java/com/example/filmapp/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.filmapp;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+// testing
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
