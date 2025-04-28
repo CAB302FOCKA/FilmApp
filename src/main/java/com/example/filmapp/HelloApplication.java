deb10c7557547bb24b61594af9b68e6b482c3df7
ohwhennn <owenasmono@gmail.com>
2025-04-28 13:32:18 +1000

    merging settings to dev

--- src/main/java/com/example/filmapp/HelloApplication.java
+++ src/main/java/com/example/filmapp/HelloApplication.java
@@ -3,7 +3,7 @@ package com.example.filmapp;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
+// Owen
 //Main method to run the whole application
 public class HelloApplication extends Application {
     @Override
