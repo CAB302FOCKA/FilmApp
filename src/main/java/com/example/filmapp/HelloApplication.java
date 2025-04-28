214c5508f7eaae75bbfbfd10a16bdbd49b78bda6
Charlie Nielsen <charlielnielsen@hotmail.com>
2025-04-28 13:07:10 +1000

    Adding comment

--- src/main/java/com/example/filmapp/HelloApplication.java
+++ src/main/java/com/example/filmapp/HelloApplication.java
@@ -3,8 +3,8 @@ package com.example.filmapp;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-// testing
 
+//Main method to run the whole application
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
