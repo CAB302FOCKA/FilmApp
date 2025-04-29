3a4b44c29570c33e4e1ce5049911cb85a8da23e6
kyhaa <kyhaanderson@gmail.com>
2025-04-29 16:26:07 +1000

    Added javadoc comments to all project classes and removed unused imports in search.fxml

--- src/main/java/com/example/filmapp/TVSeries.java
+++ src/main/java/com/example/filmapp/TVSeries.java
@@ -1,5 +1,8 @@
 package com.example.filmapp;
 
+/**
+ Unused class intended to serve as a subclass to the Media class with TV-specific attributes
+ */
 public class TVSeries {
     public TVSeries(String id, String title, String overview, String posterPath, double rating) {
     }
