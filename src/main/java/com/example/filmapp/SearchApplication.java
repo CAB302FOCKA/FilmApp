b6b908a8df669e3fd4387f8756645572ced8f15d
Charlie Nielsen <charlielnielsen@hotmail.com>
2025-05-01 03:29:35 +1000

    fixing page transfer

---
+++ src/main/java/com/example/filmapp/SearchApplication.java
@@ -0,0 +1,22 @@
+package com.example.filmapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SearchApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("search.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
