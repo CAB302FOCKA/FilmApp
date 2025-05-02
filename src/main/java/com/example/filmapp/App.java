91f01dc582746458dcca8c2b499c093da20852de
Westbrooke117 <kyhaanderson@gmail.com>
2025-05-02 13:21:23 +1000

    Made images load in background. Somewhat improving responsiveness.

--- src/main/java/com/example/filmapp/App.java
+++ src/main/java/com/example/filmapp/App.java
@@ -11,9 +11,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-//        Temporary fix for resolution issue on small screens with DPI scaling
-//        stage.setWidth(1280);
-//        stage.setHeight(720);
+        stage.setWidth(1280);
+        stage.setHeight(720);
 
         SceneManager.setStage(stage);
         SceneManager.switchTo("login.fxml");
